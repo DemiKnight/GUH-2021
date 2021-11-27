a46857b421e4dd29c5bb7eb3d5621d950c51f81c
miau <notmiau@pm.me>
2021-11-27 20:05:53 +0000

    UPD: Removed comments and unnecessary imports

--- frontend/src/Nav.tsx
+++ frontend/src/Nav.tsx
@@ -1,32 +1,9 @@
 import React from 'react'
 import './Nav.css'
 import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core/';
-// import { Nav,  NavItem } from "react-bootstrap"
-// import { IndexLinkContainer } from "react-router-bootstrap"
-// import { Link } from 'react-router-dom';
 import MenuIcon from '@material-ui/icons/Menu';
-import {Map, MapRounded, MapTwoTone} from "@material-ui/icons";
+import {MapRounded} from "@material-ui/icons";
 
-// import * as React from 'react';
-
-// function Aba() {
-//     return (
-//
-//         <div>
-//             <h1 className="nav-title">Information on Cars</h1>
-//             <nav className="nav-list">
-//
-//                 <li>Home</li>
-//                 <li>About</li>
-//                 <li>Contact</li>
-//             </nav>
-//
-//         </div>
-//
-//     )
-// }
-//
-// export default Aba
 function Nav() {
 
     return (
@@ -45,11 +22,3 @@ function Nav() {
     );
 }
 export default Nav
-
-/*
-                <Typography  color="inherit">
-                    <Link style={{color: "white"}} to="/">test</Link>
-                    <Link to="/FirstComponent"> Page 1 </Link>
-                    <Link to="/SecondComponent"> Page 2 </Link>
-                </Typography>
- */
