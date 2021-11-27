038a33308074e1bbb3041d6c6a9c6f99e0cc7a9e
Mariyan N <mariyannakev@gmail.com>
2021-11-27 16:53:15 +0000

    Navbar with materal-ui

--- frontend/src/Nav.tsx
+++ frontend/src/Nav.tsx
@@ -1,17 +1,55 @@
 import React from 'react'
 import './Nav.css'
+import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core/';
+// import { Nav,  NavItem } from "react-bootstrap"
+// import { IndexLinkContainer } from "react-router-bootstrap"
+// import { Link } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+import {Map, MapRounded, MapTwoTone} from "@material-ui/icons";
 
+// import * as React from 'react';
+
+// function Aba() {
+//     return (
+//
+//         <div>
+//             <h1 className="nav-title">Information on Cars</h1>
+//             <nav className="nav-list">
+//
+//                 <li>Home</li>
+//                 <li>About</li>
+//                 <li>Contact</li>
+//             </nav>
+//
+//         </div>
+//
+//     )
+// }
+//
+// export default Aba
 function Nav() {
+
     return (
         <div>
-            <h1 className="nav-title">Information on Cars</h1>
-            <nav className="nav-list">
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </nav>
-        </div>
-    )
-}
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton  aria-label="Menu" color="inherit">
+                    <MenuIcon aria-haspopup="true"/>
+                    <MapRounded> </MapRounded>
+                </IconButton>
 
+            </Toolbar>
+
+        </AppBar>
+        </div>
+    );
+}
 export default Nav
+
+/*
+                <Typography  color="inherit">
+                    <Link style={{color: "white"}} to="/">test</Link>
+                    <Link to="/FirstComponent"> Page 1 </Link>
+                    <Link to="/SecondComponent"> Page 2 </Link>
+                </Typography>
+ */
