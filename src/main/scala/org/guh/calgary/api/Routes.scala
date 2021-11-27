3f1566685c96aca7cdcf796f7040e4160c9c830e
Alex Knight <alex@alexknight.co.uk>
2021-11-27 17:23:40 +0000

    Improve route

--- src/main/scala/org/guh/calgary/api/Routes.scala
+++ src/main/scala/org/guh/calgary/api/Routes.scala
@@ -8,25 +8,20 @@ import org.guh.calgary.api.sysadmin.SysadminBookingRoute
 
 
 object Routes {
-
   val bookingRoute: Route = path("booking")(BookingRoute.route)
   val adminBookingRoute: Route = path("booking")(SysadminBookingRoute.route)
 
   val carRoute: Route = path("car")(CarRoute.route)
   val adminCarRoute: Route = path("car")(SysadminBookingRoute.route)
 
-  val publicPaths: Route = pathPrefix("v1") {
-    carRoute ~ bookingRoute
-  }
-
-  val adminPaths: Route = pathPrefix("sysadmin") {
-    adminBookingRoute ~ adminCarRoute
-  }
-
-  val allRoutes = Seq(
-    adminPaths,
-    carRoute
-  ).reduce((x, y) => x ~ y)
+  val allRoutes = concat(
+    pathPrefix("v1") {
+      carRoute ~ bookingRoute
+    },
+    pathPrefix("sysadmin") {
+      adminBookingRoute ~ adminCarRoute
+    }
+  )
 }
 
 
